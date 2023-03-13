b0b69a70e589fadd5eef0edfc4c04b5b7d7995d7
Bahodir To'rayev <turayev.bahodir95@gmail.com>
2023-03-13 18:03:58 +0500

    aaa

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import com.example.demo.calculator.CalculatorResponse;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/example/demo/calculator/CalculatorUtil.java
+++ src/main/java/com/example/demo/calculator/CalculatorUtil.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class CalculatorUtil {
     public List<CalculatorResponse> oddiy(double balance,
@@ -15,10 +16,11 @@ public class CalculatorUtil {
         List<CalculatorResponse> responses = new ArrayList<>();
         double asosiyQarz;
         double sum;
+        double percentage1 = percentage / 1000;
         double oylikKridit = balance / moon;
         for (int i = 0; i < moon; i++) {
             CalculatorResponse response = new CalculatorResponse();
-            sum = balance * percentage;
+            sum = balance * percentage1;
             System.out.println(asosiyQarz = oylikKridit + sum);
             response.setTotalMonthlyPayment(asosiyQarz = oylikKridit + sum);
             response.setBalance(balance);
@@ -41,26 +43,28 @@ public class CalculatorUtil {
                                              double insuranceCosts,
                                              double notaryExpenses,
                                              double anotherExpenses) {
+        double balanse1 = balance*percentage/moon*Math.pow(1+percentage/moon,moon);
+        int i=0;
         List<CalculatorResponse> responses = new ArrayList<>();
-        double asosiyQarz;
-        double sum;
-        double oylikKridit = balance / moon;
-        for (int i = 0; i < moon; i++) {
+        while (balance > 0) {
             CalculatorResponse response = new CalculatorResponse();
-            sum = balance * percentage;
-            System.out.println(asosiyQarz = oylikKridit + sum);
-            response.setTotalMonthlyPayment(asosiyQarz = oylikKridit + sum);
-            response.setBalance(balance);
-            response.setPercentage(sum);
-            asosiyQarz = oylikKridit;
-            response.setPrincipalDebt(asosiyQarz);
-            balance = balance - oylikKridit;
-            response.setAnotherExpenses(anotherExpenses);
-            response.setCommissionFee(notaryExpenses);
-            response.setPersonalExpenses(insuranceCosts);
-            response.setMoon(i + 1);
+            double p=balance*percentage/1000;
+            response.setTotalMonthlyPayment(balanse1);
+            response.setBalance(balance-p);
+            response.setMoon(i++);
             responses.add(response);
         }
         return responses;
+    }
+
+    public double sum(double balance, double percentage, double moon) {
+        double i = percentage / 100 / moon;
+        System.out.println(i);
+        double d1 = i * Math.pow(1 + i, moon);
+        System.out.println(d1);
+        double d2 = Math.pow(1 + i, moon) - 1;
+        System.out.println(d2);
+        System.out.println(balance * d1 / d2);
+        return balance * d1 / d2;
     }
 }
