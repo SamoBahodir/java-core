4b7f0983a662b72d874c078c22df5707d2224973
Bahodir <turayev.bahodir95@gmail.com>
2023-04-15 16:44:09 +0500

    java core

--- src/main/java/com/example/demo/EmoloyeeRepository.java
+++ src/main/java/com/example/demo/EmoloyeeRepository.java
@@ -17,9 +17,6 @@ public interface EmoloyeeRepository extends JpaRepository<Employee, Long> {
 
     List<Employee> findByNameEndingWith(String name);
 
-    @Query(value = "select * from clen_code where lastName between S and B", nativeQuery = true)
-    List<Employee> findByLastNameBetween(String lastName);
-
     @Query(value = "select * from clen_code where id between ?1 and ?2", nativeQuery = true)
     List<Employee> findByIdBetween(Long start, Long end);
 }
