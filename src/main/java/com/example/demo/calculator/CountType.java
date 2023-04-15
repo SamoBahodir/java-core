2d24952ce26dec83c5710048db1dc2b11b86a65c
Bahodir <turayev.bahodir95@gmail.com>
2023-04-15 16:45:50 +0500

    java core

--- src/main/java/com/example/demo/calculator/CountType.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.calculator;
-
-public enum CountType {
-    ODDIY, ANNUITET
-}
--- src/main/java/com/example/demo/calculator/CreditType.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.calculator;
-
-public enum CreditType {
-    ONLINE_MICROLOAN
-}
